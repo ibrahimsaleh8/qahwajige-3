b51640fe3892d2c9e4cdd0255f37abba95191b0a
ibrahimsaleh8 <ebrihm576@gmail.com>
2026-04-20 20:27:42 +0200

    uncomment proxy

--- proxy.ts
+++ proxy.ts
@@ -3,10 +3,10 @@ import { geolocation } from "@vercel/functions";
 import type { NextRequest } from "next/server";
 
 export function proxy(request: NextRequest) {
-  // const country = geolocation(request)?.country;
-  // if (country !== "SA") {
-  //   return NextResponse.redirect(new URL("/outside-saudi", request.url));
-  // }
+  const country = geolocation(request)?.country;
+  if (country !== "SA") {
+    return NextResponse.redirect(new URL("/outside-saudi", request.url));
+  }
 
   return NextResponse.next();
 }
