4843d64ec91ebb5d35348dd43ccd4d3276dc66f6
ibrahimsaleh8 <ebrihm576@gmail.com>
2026-04-21 22:49:03 +0200

    add code to middleware to block bots

--- proxy.ts
+++ proxy.ts
@@ -1,9 +1,30 @@
 import { NextResponse } from "next/server";
-import { geolocation } from "@vercel/functions";
+import { geolocation, ipAddress } from "@vercel/functions";
 import type { NextRequest } from "next/server";
+const blockedIPs = ["1.1.1.1", "2.2.2.2"];
+
+const blockedAgents = ["bot", "crawler", "spider", "curl", "wget", "python"];
 
 export function proxy(request: NextRequest) {
-  const country = geolocation(request)?.country;
+  const ip = ipAddress(request) || "unknown";
+
+  if (blockedIPs.includes(ip)) {
+    console.log("Blocked IP:", ip);
+    return new NextResponse("Access Denied", { status: 403 });
+  }
+  const userAgent = request.headers.get("user-agent")?.toLowerCase() || "";
+  if (!userAgent) {
+    return new NextResponse("No User-Agent", { status: 403 });
+  }
+
+  const isBot = blockedAgents.some((agent) => userAgent.includes(agent));
+
+  if (isBot) {
+    console.log("Blocked Bot:", userAgent);
+    return new NextResponse("Bot Detected", { status: 403 });
+  }
+
+  const country = geolocation(request)?.country || "UNKNOWN";
   if (country !== "SA") {
     return NextResponse.redirect(new URL("/outside-saudi", request.url));
   }
